1c9595d91c1a42cb0eeb3c168cbff2e7082da30a
max <>
2013-08-19 10:02:41 +0000

    Fixed 02-services-started.sh to check for stopped as well as crashed services. Fixed 99-lkg to check for rd.lkg.snapshot instead of rc.lkg.snapshot.

--- lib/lkg.tests/02-services-started.sh
+++ lib/lkg.tests/02-services-started.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # If any services have crashed then this test failed!
-rc-status | grep crashed && exit 1
+rc-status | grep 'crashed\|stopped' && exit 1
 
 # If we got this far then all is well.
 exit 0
